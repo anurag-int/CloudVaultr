a7712bbcf524edfe25b25a40d531bd20d5a6f63d
anurag-int <ag6526803@gmail.com>
2023-05-23 02:59:30 +0530

    cloudinary Configuration

--- server.js
+++ server.js
@@ -1,5 +1,39 @@
 const express = require('express');
 const app = express();
+const nodemailer = require('nodemailer');
+require(express.json());
+const cloudinary = require('cloudinary').v2;
+
+
+
+// Configuration 
+cloudinary.config(
+    {
+        cloud_name: "Anurag'sCloud",
+        api_key: "125582343148666" ,
+        api_secret : "qN0epvNA2yPeLtS4onDgbaFiOTo" 
+    }
+);
+
+
+
+
+const file = req.files.file;
+cloudinary.uploader.upload(file.tempFilePath, function(error, result){
+    if(error){
+        console.error(error);
+        res.status(500).send("Error uploading file to cloudinary");
+    }
+    else
+    {
+        console.log(result);
+        res.send("File uploaded to Cloudinary");
+    }
+});
+
+
+
+
 const PORT = 3000;
 app.get("/", (req, res)=>{
     res.send(`<H1>Welcome To Home Page</H1>`);
